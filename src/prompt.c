1840e7a3ae9de539dd7192be22d169dae9450df1
Kernel.rb <matabsaifeddine@gmail.com>
2024-04-24 19:35:53 +0200

    Fix some error on prompt.c

--- src/prompt.c
+++ src/prompt.c
@@ -13,11 +13,13 @@
 
 int main(int argc , char** argv){
   // TODO: ASCII ART FOR Lipsy 
-  puts( ANSI_COLOR_CYAN "------Lispy 0.0.0.1-----------" ANSI_COLOR_RESET);
-  puts(ANSI_COLOR_CYAN"------Press CTRL + C or type exit to exit --" ANSI_COLOR_RESET);
-  while(1){
-    // TODO: color the lispy 
-    char* input = readline("Lipsy>");
+  puts(ANSI_COLOR_CYAN "===== Lispy v0.0.0.1 =====" ANSI_COLOR_RESET);
+  puts(ANSI_COLOR_CYAN "===== Press CTRL + C or type 'exit' to quit =====" ANSI_COLOR_RESET);
+
+  while(1){ 
+    // TODO : fix mli knwrk 3la effacer tytms7 ta dak Lipsy 
+    printf(ANSI_COLOR_YELLOW "Lipsy> " ANSI_COLOR_RESET);
+    char *input = readline("");
     add_history(input);
     if(strcmp(input , "exit") == 0){
       exit(0);
